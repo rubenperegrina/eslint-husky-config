4bc0a0636259e724b45fa29b3f715011981d8725
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-10 12:51:22 +0100

    Init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'eslint-prettier-husky-config'`, () => {
+  it(`should have as title 'eslint-husky-config'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('eslint-prettier-husky-config');
+    expect(app.title).toEqual('eslint-husky-config');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('eslint-prettier-husky-config app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('eslint-husky-config app is running!');
   });
 });
